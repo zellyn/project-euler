42647123787cbd48eec5053724b45c382438ac2b
Zellyn Hunter <zellyn@gmail.com>
2011-06-23 18:00:30 -0700

    cc: 28

--- cc/028.cc
+++ cc/028.cc
@@ -3,7 +3,16 @@
 // What is the sum of both diagonals in a 1001 by 1001 spiral?
 
 #include <cstdio>
+#include "limits.h"
 
 int main(char argv[]) {
-  printf("%d\n", 0);
+  int sum = 1;
+  int n = 1;
+  for (int i=2; i < 1001; i += 2) {
+    for (int j=0; j<4; j++) {
+      n += i;
+      sum += n;
+    }
+  }
+  printf("%d\n", sum);
 }

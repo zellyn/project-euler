85115698268ff38e95befb51c206bc7a9418462c
Zellyn Hunter <zellyn@gmail.com>
2011-06-24 22:36:36 -0700

    cc:31

--- cc/031.cc
+++ cc/031.cc
@@ -4,6 +4,21 @@
 
 #include <cstdio>
 
+int DENOMS[] = { 200, 100, 50, 20, 10, 5, 2, 1 };
+
+int combos(int amt, int index) {
+  int denom = DENOMS[index];
+  if (denom == 1) {
+    return 1;
+  }
+
+  int sum = 0;
+  for (int i=amt/denom*denom; i >= 0; i-=denom) {
+    sum += combos(amt-i, index+1);
+  }
+  return sum;
+}
+
 int main(int argc, const char* argv[]) {
-  printf("%d\n", 0);
+  printf("%d\n", combos(200, 0));
 }

1444b33d93c007b0cda9ebed542c04ce0c5083c6
Zellyn Hunter <zellyn@gmail.com>
2011-06-25 22:56:51 -0700

    cc:41

--- cc/040.cc
+++ cc/040.cc
@@ -4,7 +4,34 @@
 // number.
 
 #include <cstdio>
+#include <cmath>
+
+int nth_digit(int n, int starts[]) {
+  int zn = n-1;
+  int i = 0;
+  while (zn >= starts[i+1]) {
+    i++;
+  }
+  int count = zn - starts[i];
+  int number = count / (i+1) + pow(10, i);
+  int div = pow(10, i - (count % (i+1)));
+  return (number / div) % 10;
+}
 
 int main(int argc, const char* argv[]) {
-  printf("%d\n", 0);
+  int starts[7] = { 0 };
+  int t1 = 1;
+  for (int i=0; i<6; i++) {
+    int count = 9 * t1;
+    starts[i+1] = starts[i] + (i+1) * count;
+    t1 *= 10;
+  }
+
+  int prod = 1;
+  int d = 10;
+  for (int i = 0; i<6; i++) {
+    prod *= nth_digit(d, starts);
+    d *= 10;
+  }
+  printf("%d\n", prod);
 }
--- cc/041.cc
+++ cc/041.cc
@@ -3,7 +3,32 @@
 // What is the largest n-digit pandigital prime that exists?
 
 #include <cstdio>
+#include "primes1.h"
+#include "mymath.h"
+#include <climits>
+
+// 1+...+8 = 36, and 1+...+9 = 45, so all 8- and 9-digit pandigitals
+// are divisible by 3.
+
+int check(int n) {
+  int d = digits(n);
+  if (d&1) return false;  // 0 not allowed
+  d >>= 1;
+  while (d) {
+    if (!(d&1)) return false;  // missing digits not allowed
+    d >>= 1;
+  }
+  return true;
+}
 
 int main(int argc, const char* argv[]) {
-  printf("%d\n", 0);
+  Primes1<int> primes;
+  int max = 0;
+  int p;
+  while ((p=primes.next()) < 1e7) {
+    if (p > 2143 && check(p)) {  // 2143 given in problem
+      max = p;
+    }
+  }
+  printf("%d\n", max);
 }

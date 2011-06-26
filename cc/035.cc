faba91755ad6f7d064731c0721999b66426ca01c
Zellyn Hunter <zellyn@gmail.com>
2011-06-25 18:01:14 -0700

    cc:35

--- cc/035.cc
+++ cc/035.cc
@@ -3,7 +3,28 @@
 // How many circular primes are there below one million?
 
 #include <cstdio>
+#include <cmath>
+#include "primes1.h"
+
+bool circular(int p, Primes1<int>& primes) {
+  if (p<10) return true;
+  int len = log10(p);
+  int shift = pow(10.0, len);
+  for (int i=0; i<len; i++) {
+    p = (p % 10) * shift + (p/10);
+    if (!primes.is_prime(p)) return false;
+  }
+  return true;
+}
 
 int main(int argc, const char* argv[]) {
-  printf("%d\n", 0);
+  Primes1<int> primes;
+  int count = 0;
+  int p;
+  while ((p=primes.next()) < 1e6) {
+    if (circular(p, primes)) {
+      count++;
+    }
+  }
+  printf("%d\n", count);
 }

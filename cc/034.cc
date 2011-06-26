7e7657df0576a0fda0bd1fa3408827ce46ad4541
Zellyn Hunter <zellyn@gmail.com>
2011-06-25 17:45:59 -0700

    cc:34

--- cc/034.cc
+++ cc/034.cc
@@ -5,6 +5,27 @@
 
 #include <cstdio>
 
+// fact(9) * 7 = 2540160, so we need go no higher
+
+int fact(int n) {
+  if (n<2) return 1;
+  return n * fact(n-1);
+}
+
+int check(int n) {
+  int sum = 0;
+  for (int i=n; i; i/=10) {
+    sum += fact(i%10);
+    if (sum >n) return 0;
+  }
+  if (sum < n) return 0;
+  return n;
+}
+
 int main(int argc, const char* argv[]) {
-  printf("%d\n", 0);
+  int sum = 0;
+  for (int i=10; i<2540160; i++) {
+    sum += check(i);
+  }
+  printf("%d\n", sum);
 }

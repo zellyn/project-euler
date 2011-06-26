424875b0694145800b516c068c1db74f85a5884f
Zellyn Hunter <zellyn@gmail.com>
2011-06-25 18:19:31 -0700

    cc:37

--- cc/036.cc
+++ cc/036.cc
@@ -5,6 +5,21 @@
 
 #include <cstdio>
 
+bool palin(int n, int base) {
+  if (!n%base) return false;
+  int rev = 0;
+  for (int i=n; i; i/=base) {
+    rev = rev * base + i%base;
+  }
+  return rev == n;
+}
+
 int main(int argc, const char* argv[]) {
-  printf("%d\n", 0);
+  int sum = 0;
+  for (int i=1; i<1e6; i++) {
+    if (palin(i, 10) && palin(i, 2)) {
+      sum += i;
+    }
+  }
+  printf("%d\n", sum);
 }
--- cc/037.cc
+++ cc/037.cc
@@ -4,7 +4,36 @@
 // left to right and right to left.
 
 #include <cstdio>
+#include "primes1.h"
+
+bool check(int n, Primes1<int>& primes) {
+  if (n<10) return false;
+  int top10 = 1;
+  for (int nn = n/10; nn; nn /= 10) {
+    if (!primes.is_prime(nn)) return false;
+    top10 *= 10;
+  }
+
+  int nn = n;
+  while (true) {
+    nn = nn - (nn/top10 * top10);
+    if (!nn) return true;
+    if (!primes.is_prime(nn)) return false;
+    top10 /= 10;
+  }
+}
 
 int main(int argc, const char* argv[]) {
-  printf("%d\n", 0);
+  Primes1<int> primes;
+  int sum = 0;
+  int count = 0;
+  while (count < 11) {
+    int p = primes.next();
+    if (check(p, primes)) {
+      sum += p;
+      count++;
+    }
+  }
+  check(3797, primes);
+  printf("%d\n", sum);
 }

c92b745a74a24489b5c126e9b76aef05ecc8c0e8
Zellyn Hunter <zellyn@gmail.com>
2011-06-27 22:46:39 -0700

    cc: 14 - brute force is fastest

--- cc/014.cc
+++ cc/014.cc
@@ -4,34 +4,27 @@
 // million.
 
 #include <cstdio>
-#include <map>
 
 using namespace std;
 
-int len(long int n, map<long int, int>& m) {
-  if (m.count(n)) {
-    return m[n];
-  }
+int len(long int n) {
+  if (n < 3) return n;
   int count = 0;
   if (n&1) {
     // Going up immediately goes down
-    count = 2 + len((3*n+1)>>1, m);
+    count = 2 + len((3*n+1)>>1);
   } else {
-    count = 1 + len(n>>1, m);
+    count = 1 + len(n>>1);
   }
-  m[n] = count;
   return count;
 }
 
 int main(char argv[]) {
   int max = 0;
   int nmax = 0;
-  map<long int, int> m;
-  m[1] = 1;
   // Best guesses are 3, 7, 11, ...
   for (long int i = 3; i < 1e6; i+=4) {
-    if (m.count(i)) continue;
-    int l = len(i, m);
+    int l = len(i);
     if (l > max) {
       max = l;
       nmax = i;

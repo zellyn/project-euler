5d0c23600b0256095c14ba3e6648d03ad091dd2b
Zellyn Hunter <zellyn@gmail.com>
2011-06-30 17:44:43 -0700

    cc:50

--- cc/050.cc
+++ cc/050.cc
@@ -4,7 +4,30 @@
 // most consecutive primes?
 
 #include <cstdio>
+#include "primes1.h"
+
 
 int main(int argc, const char* argv[]) {
-  printf("%d\n", 0);
+  int longest = 21;  // Given in problem
+  longest = 5;
+  Primes1<int> primes;
+  vector<int> sums;
+  sums.push_back(0);
+  int i = 0;
+  while ((i<longest) || (sums[i] - sums[i-longest+1] < 1e6)) {
+    sums.push_back(sums[i] + primes.next());
+    i++;
+  }
+  int sum = 0;
+  for (int j=0; j<=i-longest; j++) {
+    for (int k=j+longest; k<i; k++) {
+      int s = sums[k] - sums[j];
+      if (s>1e6) break;
+      if (!primes.is_prime(s)) continue;
+      if ((k-j) < longest) continue;
+      sum = s;
+      longest = k-j;
+    }
+  }
+  printf("%d\n", sum);
 }

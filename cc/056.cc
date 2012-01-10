8628bdfcd278bfdfd6703926cb484972a03da61c
Zellyn Hunter <zellyn@gmail.com>
2012-01-09 18:49:40 -0800

    cc:056 done

--- cc/056.cc
+++ cc/056.cc
@@ -1,10 +1,25 @@
 // Project Euler Problem 056
 //
-// Considering natural numbers of the form, ab, finding the maximum
-// digital sum.
+// Considering natural numbers of the form, a^b, where a, b < 100,
+// what is the maximum digital sum?
 
 #include <cstdio>
+#include <vector>
+
+#include "bigmath.h"
 
 int main(int argc, const char* argv[]) {
-  printf("%d\n", 0);
+  int max = 0;
+  for (int a=1; a < 100; a++) {
+    vector<int> vec;
+    vec.push_back(1);
+    times_digits_reversed(vec, a);
+    for (int b=1; b < 100; b++) {
+      vector<int> vec2 = vec;
+      pow_digits_reversed(vec2, b);
+      int sum = sum_digits(vec2);
+      if (sum > max) max = sum;
+    }
+  }
+  printf("%d\n", max);
 }
--- cc/bigmath.h
+++ cc/bigmath.h
@@ -3,23 +3,16 @@
 
 #include "limits.h"
 #include <cassert>
+#include <iostream>
 #include <vector>
+
 using namespace std;
 
-// Multiply a bigint by an int, in-place.
-// v holds the bigint, one digit per entry, with the least-significant
-// digit first.
-void times_digits_reversed(vector<int>& digits, int plier) {
-  int carry = 0;
-  for (size_t i=0; i<digits.size(); i++) {
-    int prod = digits[i] * plier + carry;
-    digits[i] = prod % 10;
-    carry = prod / 10;
+void print_digits(vector<int>& digits) {
+  for (vector<int>::reverse_iterator it=digits.rbegin(); it != digits.rend(); ++it) {
+    cout << *it;
   }
-  while (carry) {
-    digits.push_back(carry % 10);
-    carry /= 10;
-  }
+  cout << endl;
 }
 
 void addto_digits_reversed(vector<int>& target, vector<int>& add) {
@@ -49,6 +42,72 @@ void addto_digits_reversed(vector<int>& target, vector<int>& add) {
   if (carry) {
     assert (carry <= 9);
     target.push_back(carry);
+  }
+}
+
+// Multiply a bigint by an int, in-place.
+// v holds the bigint, one digit per entry, with the least-significant
+// digit first.
+void times_digits_reversed(vector<int>& digits, int plier) {
+  int carry = 0;
+  for (size_t i=0; i<digits.size(); i++) {
+    int prod = digits[i] * plier + carry;
+    digits[i] = prod % 10;
+    carry = prod / 10;
+  }
+  while (carry) {
+    digits.push_back(carry % 10);
+    carry /= 10;
+  }
+}
+
+// Multiply a bigint by a bigint, in-place.
+// digits holds the bigint, one digit per entry, with the
+// least-significant digit first, and the result.
+void times_digits_reversed(vector<int>& digits, vector<int> plier) {
+  vector<int> digits_copy = digits;
+  digits.clear();
+  int shift = 0;
+  while (plier.size()) {
+    // shift over
+    for (int i=0; i<shift; i++) {
+      digits_copy.insert(digits_copy.begin(), 0);
+    }
+    shift = 0;
+    vector<int> digits_again = digits_copy;
+
+    // Grab up to five digits of the multiplier
+    int power = 1;
+    int multiplier = 0;
+    for (int i=0; i<5; i++) {
+      if (!plier.size()) break;
+      multiplier+= power * plier[0];
+      plier.erase(plier.begin());
+      power *= 10;
+      shift++;
+    }
+
+    if (multiplier) {
+      // Multiply
+      times_digits_reversed(digits_again, multiplier);
+
+      // Add to result
+      addto_digits_reversed(digits, digits_again);
+    }
+  }
+}
+
+void pow_digits_reversed(vector<int>& digits, int power) {
+  vector<int> digits_copy = digits;
+  digits.clear();
+  digits.push_back(1);
+  while (power) {
+    if (power & 1) {
+      times_digits_reversed(digits, digits_copy);
+    }
+    power >>= 1;
+    vector<int> plier = digits_copy;
+    times_digits_reversed(digits_copy, plier);
   }
 }
 

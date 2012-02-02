2c42ed3ada5335945d8110928480c242190b4920
Zellyn Hunter <zellyn@gmail.com>
2012-02-01 16:36:46 -0800

    cc: clean some comments up

--- cc/030.cc
+++ cc/030.cc
@@ -2,7 +2,7 @@
 //
 // Find the sum of all the numbers that can be written as the sum of
 // fifth powers of their digits.
-
+//
 // 9 ** 5 * 6 = 354294, so we only have to check up to 354294.
 
 #include <cstdio>
--- cc/061.cc
+++ cc/061.cc
@@ -3,8 +3,6 @@
 // Find the sum of the only set of six 4-digit figurate numbers with a
 // cyclic property.
 
-// 3-8
-
 #include <cstdio>
 #include <map>
 #include <vector>

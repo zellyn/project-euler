2531d0fb4c7f6b038116399b594d25e4b73ad47d
Zellyn Hunter <zellyn@gmail.com>
2012-02-20 14:44:36 -0800

    go:5

--- go/005.go
+++ go/005.go
@@ -4,8 +4,30 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"./primes"
+)
 
 func main() {
-	fmt.Println(0)
+	var total int64 = 1
+	counts := make(map[int64]int)
+	for i := 2; i <= 20; i++ {
+		var last int64 = 0
+		count := 0
+		for _, factor := range primes.PrimeFactors(int64(i)) {
+			if factor == last {
+				count++
+			} else {
+				last = factor
+				count = 1
+			}
+			if counts[factor] < count {
+				counts[factor] = count
+				total *= factor
+			}
+		}
+	}
+	fmt.Println(total)
 }

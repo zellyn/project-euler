5cf6b7f495ffb7aec0d85f57fa35df31e89cd203
Zellyn Hunter <zellyn@gmail.com>
2012-02-28 20:58:49 -0800

    go:14

--- go/014.go
+++ go/014.go
@@ -7,6 +7,23 @@ package main
 
 import "fmt"
 
+func len(n uint) uint {
+	if n < 3 {
+		return n
+	}
+	if n&1 == 1 {
+		return 2 + len((3*n+1)>>1)
+	}
+	return 1 + len(n>>1)
+}
+
 func main() {
-	fmt.Println(0)
+	var max, nmax uint
+	for i := uint(3); i < 1e6; i += 4 {
+		if l := len(i); l > max {
+			max = l
+			nmax = i
+		}
+	}
+	fmt.Println(nmax)
 }

13975f7813227a1f0fe1ea1b7d50147f6d9626dd
Zellyn Hunter <zellyn@gmail.com>
2012-11-14 08:19:05 -0800

    go: p027

--- go/problems/027.go
+++ go/problems/027.go
@@ -5,12 +5,35 @@
 
 package problems
 
-import "fmt"
+import (
+	"fmt"
+
+	"../primes"
+)
+
+func PrimesFor(a, b int) int {
+	for n := 0; ; n++ {
+		if !primes.IsPrime(int64(n*n + a*n + b)) {
+			return n
+		}
+	}
+	panic("Not reached")
+}
 
 func Problem027() string {
-	return fmt.Sprintf("%d", 0)
+	nmax, prodmax := 0, 0
+	for b := 0; b < 1000; b++ {
+		// Can't be negative; figure out min a for n=3
+		for a := (-b - 9) / 3; a < 1000; a++ {
+			if n := PrimesFor(a, b); n > nmax {
+				nmax = n
+				prodmax = a * b
+			}
+		}
+	}
+	return fmt.Sprintf("%d", prodmax)
 }
 
 func init() {
-     Functions[27] = Problem027
+	Functions[27] = Problem027
 }

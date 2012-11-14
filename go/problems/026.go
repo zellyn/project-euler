4ed4ea5b936a7187528940eb95254718ea687e6d
Zellyn Hunter <zellyn@gmail.com>
2012-11-13 17:39:40 -0800

    go: p026 (copied C version)

--- go/problems/026.go
+++ go/problems/026.go
@@ -7,8 +7,35 @@ package problems
 
 import "fmt"
 
+func repeatLength(i int) int {
+	positionForQuotient := make(map[int]int)
+	index, q := 0, 1
+	for {
+		for ; q < i ; {
+			index++
+			q *= 10
+		}
+		// Non-repeating
+		if ((q % i) == 0) {
+			return 0
+		}
+		q -= i * (q/i)
+		if positionForQuotient[q] > 0 {
+			return index - positionForQuotient[q]
+		}
+		positionForQuotient[q] = index
+	}
+	return 0
+}
+
 func Problem026() string {
-	return fmt.Sprintf("%d", 0)
+	max, maxlen := 0, 0
+	for i := 7; i < 1000; i++ {
+		if length := repeatLength(i); length > maxlen {
+			maxlen, max = length, i
+		}
+	}
+	return fmt.Sprintf("%d", max)
 }
 
 func init() {

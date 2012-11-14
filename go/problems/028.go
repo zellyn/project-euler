18f1152687014b0f6633a08756af485664e682a8
Zellyn Hunter <zellyn@gmail.com>
2012-11-14 08:24:42 -0800

    go: 28

--- go/problems/028.go
+++ go/problems/028.go
@@ -7,7 +7,17 @@ package problems
 import "fmt"
 
 func Problem028() string {
-	return fmt.Sprintf("%d", 0)
+	sum := 1
+	inc := 2
+	n := 1
+	for i := 0; i < 1001/2; i++ {
+		for j := 0; j < 4; j++ {
+			n += inc
+			sum += n
+		}
+		inc += 2
+	}
+	return fmt.Sprintf("%d", sum)
 }
 
 func init() {

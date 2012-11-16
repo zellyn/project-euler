2312ad7322dce8c680c7082f7b3a48507d0d2212
Zellyn Hunter <zellyn@gmail.com>
2012-11-15 18:38:34 -0800

    go: p031

--- go/problems/031.go
+++ go/problems/031.go
@@ -6,8 +6,21 @@ package problems
 
 import "fmt"
 
+var denominations = []int{200, 100, 50, 20, 10, 5, 2, 1}
+
+func WaysToChange(target int, coins []int) (result int) {
+	if len(coins) == 1 || target == 0 {
+		return 1
+	}
+	for i := 0; i <= target/coins[0]; i++ {
+		rest := coins[1:]
+		result += WaysToChange(target-i*coins[0], rest)
+	}
+	return
+}
+
 func Problem031() string {
-	return fmt.Sprintf("%d", 0)
+	return fmt.Sprintf("%d", WaysToChange(200, denominations))
 }
 
 func init() {

4c72e82e672d25c3f7000e6699b4c4a5f1a1cabe
Zellyn Hunter <zellyn@gmail.com>
2012-11-15 21:33:32 -0800

    go: p034

--- go/problems/034.go
+++ go/problems/034.go
@@ -2,13 +2,33 @@
 //
 // Find the sum of all numbers which are equal to the sum of the
 // factorial of their digits.
+//
+// 9! == 362880
+// 9! * 7 == 2540160, so 7 digits max
+// 1999999: 1 + 6 * 9! == 2177281
 
 package problems
 
 import "fmt"
 
+var factorials = []int{1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880}
+
+func Check(n int) bool {
+	sum, nn := 0, n
+	for ; nn > 0; nn /= 10 {
+		sum += factorials[nn%10]
+	}
+	return sum == n
+}
+
 func Problem034() string {
-	return fmt.Sprintf("%d", 0)
+	sum := 0
+	for n := 10; n < 2177281; n++ {
+		if Check(n) {
+			sum += n
+		}
+	}
+	return fmt.Sprintf("%d", sum)
 }
 
 func init() {

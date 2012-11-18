c208e54f2328957902c918fc37b4f9912a61e7de
Zellyn Hunter <zellyn@gmail.com>
2012-11-17 22:18:20 -0800

    go: 039

--- go/problems/039.go
+++ go/problems/039.go
@@ -5,10 +5,27 @@
 
 package problems
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func Problem039() string {
-	return fmt.Sprintf("%d", 0)
+	n, max := 0, 0
+	counts := make(map[int]int)
+	for a := 1; a < 333; a++ {
+		for b := a + 1; b < 500; b++ {
+			sq := a*a + b*b
+			c := int(math.Sqrt(float64(sq)))
+			if a + b + c <= 1000 && a*a + b*b == c*c {
+				counts[a+b+c] += 1
+				if counts[a+b+c] > max {
+					n, max = a+b+c, counts[a+b+c]
+				}
+			}
+		}
+	}
+	return fmt.Sprintf("%d", n)
 }
 
 func init() {

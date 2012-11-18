7b8a6caeda46cbe293555db4cb34cbdb2a762097
Zellyn Hunter <zellyn@gmail.com>
2012-11-17 21:54:40 -0800

    go: 038

--- go/problems/038.go
+++ go/problems/038.go
@@ -5,10 +5,46 @@
 
 package problems
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
+
+func updateMaskAndNum(mask, fullNum, newNum int) (int, int) {
+	pow10 := int(math.Pow10(int(math.Log10(float64(newNum)))))
+	for newNum > 0 {
+		digit := newNum / pow10
+		bit := 1 << uint(digit)
+		if mask&bit > 0 {
+			return 0, 0
+		}
+		mask |= bit
+		fullNum = fullNum*10 + digit
+		newNum -= pow10 * digit
+		pow10 /= 10
+	}
+	return mask, fullNum
+}
 
 func Problem038() string {
-	return fmt.Sprintf("%d", 0)
+	max := 0
+	for i := 1; i < 9876; i++ {
+		mask := 1
+		num := 0
+		for n := 1; ; n++ {
+			mask, num = updateMaskAndNum(mask, num, i*n)
+			if mask == 0 {
+				break
+			}
+			if mask == 0x3ff {
+				if num > max {
+					max = num
+				}
+				break
+			}
+		}
+	}
+	return fmt.Sprintf("%d", max)
 }
 
 func init() {

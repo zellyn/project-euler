7915d2bd4072afde97bf778049f85750e9c2364c
Zellyn Hunter <zellyn@gmail.com>
2012-11-17 22:55:20 -0800

    go: p040,041

--- go/problems/040.go
+++ go/problems/040.go
@@ -8,7 +8,38 @@ package problems
 import "fmt"
 
 func Problem040() string {
-	return fmt.Sprintf("%d", 0)
+	c := make(chan int)
+	quit := make(chan bool)
+
+	go func() {
+		pow10 := 1
+		for n := 1; ; n++ {
+			if n >= pow10*10 {
+				pow10 *= 10
+			}
+			for nn, pp := n, pow10; pp > 0; {
+				digit := nn / pp
+				nn -= pp * digit
+				pp /= 10
+				select {
+				case <-quit:
+					return
+				case c <- digit:
+				}
+			}
+		}
+	}()
+
+	prod, pos := 1, 1
+	for i := 1; i < 1000001; i++ {
+		digit := <-c
+		if i == pos {
+			prod *= digit
+			pos *= 10
+		}
+	}
+	quit <- true
+	return fmt.Sprintf("%d", prod)
 }
 
 func init() {
--- go/problems/041.go
+++ go/problems/041.go
@@ -4,10 +4,35 @@
 
 package problems
 
-import "fmt"
+import (
+	"fmt"
+
+	"../primes"
+)
+
+func pandigitalN(n int64) bool {
+	mask := 0
+	count := uint(0)
+	for ; n > 0; n /= 10 {
+		mask |= (1 << uint(n%10))
+		count++
+	}
+	return mask == (1<<(count+1) - 2)
+}
 
 func Problem041() string {
-	return fmt.Sprintf("%d", 0)
+	max := int64(0)
+	for n := 0; ; n++ {
+		p := primes.Get(n)
+		// sum(1..9) == 45, so 1..8 and 1..9 are divisible by 3
+		if p > 7654321 {
+			break
+		}
+		if pandigitalN(p) {
+			max = p
+		}
+	}
+	return fmt.Sprintf("%d", max)
 }
 
 func init() {

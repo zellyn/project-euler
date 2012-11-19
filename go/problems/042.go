712b473da2523a8e4f9ec691074f2bd09ff93b8d
Zellyn Hunter <zellyn@gmail.com>
2012-11-18 20:50:43 -0800

    go: p042

--- go/problems/042.go
+++ go/problems/042.go
@@ -7,8 +7,39 @@ package problems
 
 import "fmt"
 
+func wordSum(s string) int {
+	sum := 0
+	for _, c := range s {
+		sum += int(c-'A') + 1
+	}
+	return sum
+}
+
+func isTriangular(c int) bool {
+	for n := 1; ; n++ {
+		t := n * (n + 1) / 2
+		if t > c {
+			return false
+		}
+		if t == c {
+			return true
+		}
+	}
+	panic("Unreachable")
+}
+
 func Problem042() string {
-	return fmt.Sprintf("%d", 0)
+	words, err := readNames("../data/p042_words.txt")
+	if err != nil {
+		return err.Error()
+	}
+	count := 0
+	for _, word := range words {
+		if isTriangular(wordSum(word)) {
+			count++
+		}
+	}
+	return fmt.Sprintf("%d", count)
 }
 
 func init() {

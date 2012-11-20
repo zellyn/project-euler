6f5a894b8ad5e7dda1cac00eac5ef90d72a58053
Zellyn Hunter <zellyn@gmail.com>
2012-11-20 08:09:20 -0800

    go: p044

--- go/problems/044.go
+++ go/problems/044.go
@@ -5,10 +5,36 @@
 
 package problems
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
+
+func isPentagonal(p int) bool {
+	n := int(math.Sqrt(float64(2 * p / 3)))
+	for ; ; n++ {
+		pp := n * (3*n - 1) / 2
+		if pp > p {
+			return false
+		}
+		if pp == p {
+			return true
+		}
+	}
+	panic("impossible!")
+}
 
 func Problem044() string {
-	return fmt.Sprintf("%d", 0)
+	for k := 1; ; k++ {
+		pk := k * (3*k - 1) / 2
+		for j := 1; j < k; j++ {
+			pj := j * (3*j - 1) / 2
+			if isPentagonal(pk-pj) && isPentagonal(pk+pj) {
+				return fmt.Sprintf("%d", pk-pj)
+			}
+		}
+	}
+	panic("Cannot reach")
 }
 
 func init() {

83ee6117079e388310f25aa5b9058d528ff4038f
Zellyn Hunter <zellyn@gmail.com>
2012-11-20 08:25:17 -0800

    go: p045

--- go/problems/045.go
+++ go/problems/045.go
@@ -5,10 +5,47 @@
 
 package problems
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
+
+func isTriangular64(t int64) bool {
+	n := int64(math.Sqrt(float64(2 * t)))
+	for ; ; n-- {
+		tt := n * (n + 1) / 2
+		if tt < t {
+			return false
+		}
+		if tt == t {
+			return true
+		}
+	}
+	panic("impossible!")
+}
+
+func isHexagonal64(h int64) bool {
+	n := int64(math.Sqrt(float64(h / 2)))
+	for ; ; n++ {
+		hh := n * (2*n - 1)
+		if hh > h {
+			return false
+		}
+		if hh == h {
+			return true
+		}
+	}
+	panic("impossible!")
+}
 
 func Problem045() string {
-	return fmt.Sprintf("%d", 0)
+	for n := int64(166); ; n++ {
+		p := n * (3*n - 1) / 2
+		if isTriangular64(p) && isHexagonal64(p) {
+			return fmt.Sprintf("%d", p)
+		}
+	}
+	panic("reached")
 }
 
 func init() {

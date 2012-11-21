d1c8c9060b008c2f3d106ef6bb19b7547ae262bb
Zellyn Hunter <zellyn@gmail.com>
2012-11-20 17:25:41 -0800

    go: p046

--- go/problems/046.go
+++ go/problems/046.go
@@ -5,10 +5,31 @@
 
 package problems
 
-import "fmt"
+import (
+	"fmt"
+
+	"../primes"
+)
+
+func isGoldbachException(n int) bool {
+	if primes.IsPrime(n) {
+		return false
+	}
+	for i2, inc := 0, 2; i2 < n; i2, inc = i2+inc, inc+4 {
+		if primes.IsPrime(n - i2) {
+			return false
+		}
+	}
+	return true
+}
 
 func Problem046() string {
-	return fmt.Sprintf("%d", 0)
+	for n := 37; ; n += 2 {
+		if isGoldbachException(n) {
+			return fmt.Sprint(n)
+		}
+	}
+	panic("unreachable")
 }
 
 func init() {

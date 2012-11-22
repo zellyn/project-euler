41770765eae9be1835c2d58317403747a007ffbf
Zellyn Hunter <zellyn@gmail.com>
2012-11-21 19:06:25 -0800

    go: p049

--- go/problems/049.go
+++ go/problems/049.go
@@ -5,10 +5,59 @@
 
 package problems
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+
+	"../primes"
+)
+
+func lowestPermutation(n int) (result int) {
+	digits := []int{}
+	for ; n > 0; n /= 10 {
+		digits = append(digits, n%10)
+	}
+	sort.Ints(digits)
+	for _, digit := range digits {
+		result = result*10 + digit
+	}
+	return result
+}
 
 func Problem049() string {
-	return fmt.Sprintf("%d", 0)
+	permutations := make(map[int][]int)
+	n := 25
+	for ; ; n++ {
+		p := primes.Get(n)
+		if p < 1000 {
+			continue
+		}
+		if p > 9999 {
+			break
+		}
+		lowest := lowestPermutation(p)
+		permutations[lowest] = append(permutations[lowest], p)
+	}
+	for _, v := range permutations {
+		l := len(v)
+		if l < 3 {
+			continue
+		}
+		sort.Ints(v)
+		for i := 0; i < l-2; i++ {
+			for j := i + 1; j < l-1; j++ {
+				if v[i] == 1487 && v[j] == 4817 {
+					continue
+				}
+				for k := j + 1; k < l; k++ {
+					if v[k]-v[j] == v[j]-v[i] {
+						return fmt.Sprintf("%d%d%d", v[i], v[j], v[k])
+					}
+				}
+			}
+		}
+	}
+	return fmt.Sprint(0)
 }
 
 func init() {

b2670e5cd011f68e5058369e6bf518e9f9358a27
Zellyn Hunter <zellyn@gmail.com>
2012-11-21 19:26:56 -0800

    go: p050

--- go/problems/050.go
+++ go/problems/050.go
@@ -5,10 +5,38 @@
 
 package problems
 
-import "fmt"
+import (
+	"fmt"
+
+	"../primes"
+)
 
 func Problem050() string {
-	return fmt.Sprintf("%d", 0)
+	sums := []int{}
+	sum := 0
+	for n := 0; ; n++ {
+		if p := primes.Get(n); p < 1000000 {
+			sum += p
+			sums = append(sums, sum)
+		} else {
+			break
+		}
+	}
+	longest := 0
+	prime := 0
+	for i := 0; i < len(sums)-1; i++ {
+		for j := i + 1 + longest; j < len(sums); j++ {
+			sum := sums[j] - sums[i]
+			if sum >= 1000000 {
+				break
+			}
+			if primes.IsPrime(sum) {
+				longest = j - i
+				prime = sum
+			}
+		}
+	}
+	return fmt.Sprint(prime)
 }
 
 func init() {

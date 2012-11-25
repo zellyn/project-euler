171dd8b0f56b377e5bc0350431ae925827fbe5d9
Zellyn Hunter <zellyn@gmail.com>
2012-11-25 18:04:38 -0500

    go: 53

--- go/problems/053.go
+++ go/problems/053.go
@@ -6,8 +6,25 @@ package problems
 
 import "fmt"
 
+func choicesGt1e6(n int) int {
+	choices := 1
+	for r := 1; r <= n/2; r++ { // Halfway only: it's a mirror-image
+		choices *= (n - r + 1)
+		choices /= r
+		if choices > 1000000 {
+			// All the rest (on both sides of the halfway mark)
+			return n + 1 - r*2
+		}
+	}
+	return 0
+}
+
 func Problem053() string {
-	return fmt.Sprintf("%d", 0)
+	count := 0
+	for n := 23; n <= 100; n++ {
+		count += choicesGt1e6(n)
+	}
+	return fmt.Sprint(count)
 }
 
 func init() {

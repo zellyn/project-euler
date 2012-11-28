d18e88f9520578cd6f868a4f2b9ee434d9e09c38
Zellyn Hunter <zellyn@gmail.com>
2012-11-27 21:12:22 -0800

    go:56

--- go/problems/056.go
+++ go/problems/056.go
@@ -5,10 +5,34 @@
 
 package problems
 
-import "fmt"
+import (
+	"fmt"
+	"math/big"
+)
+
+func bigDigitSum(n *big.Int) int {
+	sum := 0
+	for _, c := range n.String() {
+		sum += int(c - '0')
+	}
+	return sum
+}
 
 func Problem056() string {
-	return fmt.Sprintf("%d", 0)
+	max := 0
+	for a := int64(2); a < 100; a++ {
+		bigA := big.NewInt(a)
+		for b := int64(2); b < 100; b++ {
+			bigB := big.NewInt(b)
+			z := big.NewInt(0)
+			z.Exp(bigA, bigB, nil)
+			sum := bigDigitSum(z)
+			if sum > max {
+				max = sum
+			}
+		}
+	}
+	return fmt.Sprint(max)
 }
 
 func init() {

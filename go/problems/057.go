058b5ad323a45fefb628f68acbb0cbd899c98791
Zellyn Hunter <zellyn@gmail.com>
2012-11-28 21:06:03 -0800

    go:57

--- go/problems/057.go
+++ go/problems/057.go
@@ -5,10 +5,24 @@
 
 package problems
 
-import "fmt"
+import (
+	"fmt"
+	"math/big"
+)
 
 func Problem057() string {
-	return fmt.Sprintf("%d", 0)
+	count := 0
+	numerator, denominator, tmp := big.NewInt(3), big.NewInt(2), big.NewInt(0)
+	for i := 0; i < 1000; i++ {
+		tmp.Set(numerator)
+		numerator.Add(numerator, denominator)
+		numerator.Add(numerator, denominator)
+		denominator.Add(denominator, tmp)
+		if len(numerator.String()) > len(denominator.String()) {
+			count++
+		}
+	}
+	return fmt.Sprint(count)
 }
 
 func init() {

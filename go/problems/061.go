e097226fac2738b1c0c0366d6dba4bd7a0f80863
Zellyn Hunter <zellyn@gmail.com>
2012-12-09 21:08:01 -0800

    go: 61

--- go/problems/061.go
+++ go/problems/061.go
@@ -5,10 +5,58 @@
 
 package problems
 
-import "fmt"
+import (
+	"fmt"
+)
+
+type suffix struct {
+	digits int
+	mask   int
+}
+
+type suffixMap map[int][]suffix
+
+func search(nextDigits, endDigits, used int, info suffixMap) int {
+	if used == 0x1f {
+		if nextDigits == endDigits {
+			return 0
+		}
+		return -1
+	}
+	for _, suffix := range info[nextDigits] {
+		if suffix.mask&used > 0 {
+			continue
+		}
+		if sub := search(suffix.digits, endDigits, used|suffix.mask, info); sub > -1 {
+			return nextDigits*100 + suffix.digits + sub
+		}
+	}
+	return -1
+}
 
 func Problem061() string {
-	return fmt.Sprintf("%d", 0)
+	info := make(suffixMap)
+	for n := 1; n*(n+1)/2 <= 9999; n++ {
+		for i := 0; i < 5; i++ {
+			f := n * ((i+1)*n + 1 - i) / 2
+			if f > 9999 {
+				break
+			}
+			if f < 1000 {
+				continue
+			}
+			info[f/100] = append(info[f/100], suffix{f % 100, 1 << uint(i)})
+		}
+	}
+	// oct(19): 1045
+	// oct(58): 9976
+	for n := 19; n <= 58; n++ {
+		oct := n * (3*n - 2)
+		if s := search(oct%100, oct/100, 0, info); s > 0 {
+			return fmt.Sprint(s + oct)
+		}
+	}
+	return fmt.Sprint(0)
 }
 
 func init() {
--- go/sequence/figurate.go
+++ go/sequence/figurate.go
@@ -18,6 +18,25 @@ func IsTriangular64(t int64) bool {
 	panic("impossible!")
 }
 
+func IsSquare64(s int64) bool {
+	sqrt := int64(math.Sqrt(float64(s)))
+	return sqrt*sqrt == s
+}
+
+func IsPentagonal(p int64) bool {
+	n := int64(math.Sqrt(2 / 3 * float64(p)))
+	for ; ; n++ {
+		pp := n * (3*n - 1) / 2
+		if pp > p {
+			return false
+		}
+		if pp == p {
+			return true
+		}
+	}
+	panic("impossible")
+}
+
 func IsHexagonal64(h int64) bool {
 	n := int64(math.Sqrt(float64(h / 2)))
 	for ; ; n++ {
@@ -26,6 +45,34 @@ func IsHexagonal64(h int64) bool {
 			return false
 		}
 		if hh == h {
+			return true
+		}
+	}
+	panic("impossible!")
+}
+
+func IsHeptagonal64(h int64) bool {
+	n := int64(math.Sqrt(2 / 5 * float64(h)))
+	for ; ; n++ {
+		hh := n * (5*n - 3) / 2
+		if hh > h {
+			return false
+		}
+		if hh == h {
+			return true
+		}
+	}
+	panic("impossible")
+}
+
+func IsOctagonal64(o int64) bool {
+	n := int64(math.Sqrt(float64(o / 3)))
+	for ; ; n++ {
+		oo := n * (3*n - 2)
+		if oo > o {
+			return false
+		}
+		if oo == o {
 			return true
 		}
 	}

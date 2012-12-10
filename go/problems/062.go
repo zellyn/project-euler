47c9a9f4bd31b29255401a274bbdf00d7b0eea09
Zellyn Hunter <zellyn@gmail.com>
2012-12-10 08:17:38 -0800

    go:62

--- go/problems/062.go
+++ go/problems/062.go
@@ -5,10 +5,38 @@
 
 package problems
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
+
+func highestPermutation64(n int64) (result int64) {
+	digits := []int{}
+	for ; n > 0; n /= 10 {
+		digits = append(digits, int(n%10))
+	}
+	sort.Ints(digits)
+	for i := range digits {
+		result = result*10 + int64(digits[len(digits)-1-i])
+	}
+	return result
+}
 
 func Problem062() string {
-	return fmt.Sprintf("%d", 0)
+	smallestCubes := make(map[int64]int64)
+	cubeCounts := make(map[int64]int)
+	for n := int64(0); ; n++ {
+		cube := n * n * n
+		key := highestPermutation64(cube)
+		if smallestCubes[key] == 0 {
+			smallestCubes[key] = cube
+		}
+		cubeCounts[key]++
+		if cubeCounts[key] == 5 {
+			return fmt.Sprint(smallestCubes[key])
+		}
+	}
+	panic("unreachable")
 }
 
 func init() {

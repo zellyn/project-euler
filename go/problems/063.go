cf0254a5ef399957666996573eec3a16a987a675
Zellyn Hunter <zellyn@gmail.com>
2012-12-10 08:33:51 -0800

    go:63

--- go/problems/063.go
+++ go/problems/063.go
@@ -3,12 +3,33 @@
 // How many n-digit positive integers exist which are also an nth
 // power?
 
+// 9**21 is the last time 9**n has n digits. (9**22 is 21 digits long)
+// 10**n always has n+1 digits
+
 package problems
 
-import "fmt"
+import (
+	"fmt"
+	"math/big"
+)
 
 func Problem063() string {
-	return fmt.Sprintf("%d", 0)
+	count := 1 // 1**1
+	for base := int64(2); base <= 9; base++ {
+		bigBase := big.NewInt(base)
+		for n := 1; n <= 21; n++ {
+			bigPower := big.NewInt(int64(n))
+			bigPower.Exp(bigBase, bigPower, nil)
+			l := len(bigPower.String())
+			if l < n {
+				break
+			}
+			if l == n {
+				count++
+			}
+		}
+	}
+	return fmt.Sprint(count)
 }
 
 func init() {

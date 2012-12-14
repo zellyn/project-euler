ba3e40f72110fca35e9429bd49eb69a380158be5
Zellyn Hunter <zellyn@gmail.com>
2012-12-13 17:13:18 -0800

    go:65

---
+++ go/contfrac/sequences.go
@@ -0,0 +1,51 @@
+package contfrac
+
+import (
+	"math/big"
+)
+
+func E() (c chan int) {
+	c = make(chan int)
+	go func() {
+		c <- 2
+		i := 2
+		for {
+			c <- 1
+			c <- i
+			c <- 1
+			i += 2
+		}
+	}()
+	return c
+}
+
+// Enumerate the convergents of a continued fraction, using
+// http://en.wikipedia.org/wiki/Fundamental_recurrence_formulas
+func Convergents(in chan int) (out chan [2]*big.Int) {
+	out = make(chan [2]*big.Int)
+
+	go func() {
+		b_nm1 := big.NewInt(int64(<-in))
+		A_nm1 := big.NewInt(0).Set(b_nm1)
+		B_nm1 := big.NewInt(1)
+		out <- [2]*big.Int{A_nm1, B_nm1}
+
+		b_n := big.NewInt(int64(<-in))
+		A_n := big.NewInt(0).Mul(b_n, b_nm1)
+		A_n.Add(A_n, big.NewInt(1))
+		B_n := big.NewInt(0).Set(b_n)
+		out <- [2]*big.Int{A_n, B_n}
+
+		for {
+			b_np1 := big.NewInt(int64(<-in))
+			A_np1 := big.NewInt(0).Mul(b_np1, A_n)
+			A_np1.Add(A_np1, A_nm1)
+			B_np1 := big.NewInt(0).Mul(b_np1, B_n)
+			B_np1.Add(B_np1, B_nm1)
+			out <- [2]*big.Int{A_np1, B_np1}
+			A_nm1, B_nm1, b_nm1 = A_n, B_n, b_n
+			A_n, B_n, b_n = A_np1, B_np1, b_np1
+		}
+	}()
+	return out
+}
--- go/problems/065.go
+++ go/problems/065.go
@@ -5,10 +5,28 @@
 
 package problems
 
-import "fmt"
+import (
+	"fmt"
+	"math/big"
+
+	"../contfrac"
+)
+
+func bigDigitCount(n *big.Int) (count int) {
+	for _, c := range n.String() {
+		count += int(c - '0')
+	}
+	return count
+}
 
 func Problem065() string {
-	return fmt.Sprintf("%d", 0)
+	c := contfrac.Convergents(contfrac.E())
+	var d *big.Int
+	for i := 0; i < 100; i++ {
+		is := <-c
+		d = is[0]
+	}
+	return fmt.Sprint(bigDigitCount(d))
 }
 
 func init() {
